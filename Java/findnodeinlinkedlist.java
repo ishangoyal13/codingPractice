64a37af61ab82c7ccfbd23121d396d634b5664af
Abhinandan Goyal <abhinandan0323.cse19@chitkara.edu.in>
2021-10-04 10:48:46 +0530

    Add files via upload

---
+++ Java/findnodeinlinkedlist.java
@@ -0,0 +1,89 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class LinkedListNode<T> {
+    T data;
+    LinkedListNode<T> next;
+    
+    public LinkedListNode(T data) {
+        this.data = data;
+    }
+}
+
+public class Solution
+{
+    public static LinkedListNode<Integer> deleteNode(LinkedListNode<Integer> head, int i)
+    {
+        if(head==null )
+            return head;
+        if(i==0)
+            return head.next;
+        int count=0;
+        LinkedListNode<Integer> temp=head;
+        while(temp!=null && count<i-1)
+        {
+            temp=temp.next;
+            count++;
+        }
+        if(temp==null)
+            return head;
+        if(temp.next!=null)
+            temp.next=temp.next.next;
+
+
+        return head;
+    }
+}
+
+
+public class Runner {
+    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    public static LinkedListNode<Integer> takeInput() throws IOException {
+        LinkedListNode<Integer> head = null, tail = null;
+
+        String[] datas = br.readLine().trim().split("\\s");
+
+        int i = 0;
+        while(i < datas.length && !datas[i].equals("-1")) {
+            int data = Integer.parseInt(datas[i]);
+            LinkedListNode<Integer> newNode = new LinkedListNode<Integer>(data);
+            if(head == null) {
+                head = newNode;
+                tail = newNode;
+            }
+            else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+            i += 1;
+        }
+
+        return head;
+    }
+    
+    public static void print(LinkedListNode<Integer> head){
+        while(head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+        
+        System.out.println();
+    }
+    
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t > 0) {
+            LinkedListNode<Integer> head = takeInput(); 
+
+            int pos = Integer.parseInt(br.readLine().trim());
+            head = Solution.deleteNode(head, pos);
+            print(head);
+
+            t -= 1;
+        }
+    }
+}
